a4666c43f7f6427e7b380ae70d14de94c3d6df51
Danny <dduo0004@student.monash.edu>
2024-08-28 23:24:03 +1000

    sprite flips

--- Generic Unity Project/Assets/Scripts/PlayerCharacter/Horizontal_MOV.cs
+++ Generic Unity Project/Assets/Scripts/PlayerCharacter/Horizontal_MOV.cs
@@ -6,16 +6,29 @@ public class Horizontal_MOV : MonoBehaviour
 {
     
     private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    
     public float speed = 5;
         
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
         float horizontalDirection = Input.GetAxis("Horizontal");
         _rigidbody2D.velocity = new Vector2(horizontalDirection * speed, _rigidbody2D.velocity.y);
+        
+        if (horizontalDirection > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (horizontalDirection < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        
     }
 }
